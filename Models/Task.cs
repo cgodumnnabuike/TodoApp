61d1c89a86e82911a9bd5393b63259b778b214e8
cgodumnnabuike <ebubeodum@gmail.com>
2024-09-12 23:01:30 +0100

    Added User and Task Model

---
+++ Models/Task.cs
@@ -0,0 +1,13 @@
+﻿namespace TodoApp.Models
+{
+    public class Task
+    {
+        public int task_id { get; set; }
+        public int user_id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+}
