5b48dd0ffed8e9db1c45b16122ad88d6be1d622b
cgodumnnabuike <114875548+cgodumnnabuike@users.noreply.github.com>
2024-09-19 00:27:28 +0100

    UserId field removed from all views

--- Models/TodoTask.cs
+++ Models/TodoTask.cs
@@ -1,10 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace TodoApp.Models
 {
     public class TodoTask
     {
         public int TaskId  { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
         public int UserId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
