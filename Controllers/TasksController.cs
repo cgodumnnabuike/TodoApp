34093b2a32efc3a69f6fcbce1f3509957fa747dc
cgodumnnabuike <114875548+cgodumnnabuike@users.noreply.github.com>
2024-09-20 00:31:31 +0100

    Solved task creation issue.

--- Controllers/TasksController.cs
+++ Controllers/TasksController.cs
@@ -54,7 +54,7 @@ namespace TodoApp.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("TaskId,UserId,Title,Description,DueDate,IsCompleted")] System.Threading.Tasks.Task task)
+        public async Task<IActionResult> Create([Bind("TaskId,UserId,Title,Description,DueDate,IsCompleted")] TodoTask task)
         {
             if (ModelState.IsValid)
             {
